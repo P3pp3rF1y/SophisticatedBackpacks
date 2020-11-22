b002cc79cbebc8b01329f0a6dc72de62a6dfd1dd
P3pp3rF1y <p3pp3rf1y@gmail.com>
2020-11-22 08:58:05 +0100

    feat: Added tooltips to upgrade items to describe a bit of what they do

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/gui/TranslationHelper.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/gui/TranslationHelper.java
@@ -1,17 +1,25 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.client.gui;
 
+import net.minecraft.client.resources.I18n;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+
+import javax.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.List;
 
 @OnlyIn(Dist.CLIENT)
 public class TranslationHelper {
 	private TranslationHelper() {}
 
-	private static final String UPGRADE_PREFIX = "gui.sophisticatedbackpacks.upgrades.";
-	private static final String UPGRADE_BUTTONS_PREFIX = UPGRADE_PREFIX + "buttons.";
+	private static final String GUI_UPGRADE_PREFIX = "gui.sophisticatedbackpacks.upgrades.";
+	private static final String ITEM_UPGRADE_PREFIX = "item.sophisticatedbackpacks.";
+	private static final String UPGRADE_BUTTONS_PREFIX = GUI_UPGRADE_PREFIX + "buttons.";
 
 	public static String translUpgrade(String upgradeName) {
-		return UPGRADE_PREFIX + upgradeName;
+		return GUI_UPGRADE_PREFIX + upgradeName;
 	}
 
 	public static String translUpgradeTooltip(String upgradeName) {
@@ -20,5 +28,28 @@ public class TranslationHelper {
 
 	public static String translUpgradeButton(String buttonName) {
 		return UPGRADE_BUTTONS_PREFIX + buttonName;
+	}
+
+	public static String translUpgradeItemTooltip(String upgradeName) {
+		return ITEM_UPGRADE_PREFIX + upgradeName + ".tooltip";
+	}
+
+	public static List<StringTextComponent> getTranslatedLines(String translateKey, @Nullable Object parameters, TextFormatting... textFormattings) {
+		List<StringTextComponent> ret = getTranslatedLines(translateKey, parameters);
+		ret.forEach(l -> l.mergeStyle(textFormattings));
+		return ret;
+	}
+
+	public static List<StringTextComponent> getTranslatedLines(String translateKey, @Nullable Object parameters) {
+		String text = I18n.format(translateKey, parameters);
+
+		String[] lines = text.split("\n");
+
+		List<StringTextComponent> ret = new ArrayList<>();
+		for (String line : lines) {
+			ret.add(new StringTextComponent(line));
+		}
+
+		return ret;
 	}
 }
--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/UpgradeItemBase.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/UpgradeItemBase.java
@@ -1,11 +1,31 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.upgrades;
 
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraft.world.World;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.p3pp3rf1y.sophisticatedbackpacks.api.IBackpackUpgradeItem;
 import net.p3pp3rf1y.sophisticatedbackpacks.api.IUpgradeWrapper;
 import net.p3pp3rf1y.sophisticatedbackpacks.items.ItemBase;
 
+import javax.annotation.Nullable;
+import java.util.List;
+
+import static net.p3pp3rf1y.sophisticatedbackpacks.client.gui.TranslationHelper.getTranslatedLines;
+import static net.p3pp3rf1y.sophisticatedbackpacks.client.gui.TranslationHelper.translUpgradeItemTooltip;
+
 public abstract class UpgradeItemBase<T extends IUpgradeWrapper> extends ItemBase implements IBackpackUpgradeItem<T> {
-	public UpgradeItemBase() {
+	protected UpgradeItemBase() {
 		super(new Properties().maxStackSize(1));
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@Override
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
+		//noinspection ConstantConditions - item is registered at this point and thus registry name can't be null
+		tooltip.addAll(getTranslatedLines(translUpgradeItemTooltip(stack.getItem().getRegistryName().getPath()), null, TextFormatting.DARK_GRAY));
 	}
 }
