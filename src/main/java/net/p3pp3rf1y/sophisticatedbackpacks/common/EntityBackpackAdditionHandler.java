b443cc6bf61e4fc91cf9d8bff32683d8757ddc65
P3pp3rF1y <p3pp3rf1y@gmail.com>
2021-03-08 22:19:04 +0100

    fix: 🐛 Fixed monsters with backpacks getting unkillable when they got too high level of resistance. Also fixed applying health boost based on backpack level.

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/EntityBackpackAdditionHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/EntityBackpackAdditionHandler.java
@@ -142,10 +142,11 @@ public class EntityBackpackAdditionHandler {
 		}
 		ModifiableAttributeInstance maxHealth = monster.getAttribute(Attributes.MAX_HEALTH);
 		if (maxHealth != null) {
-			double healthAddition = maxHealth.getBaseValue() * 0.5D * minDifficulty;
+			double healthAddition = maxHealth.getBaseValue() * minDifficulty;
 			if (healthAddition > 0.1D) {
 				maxHealth.applyPersistentModifier(new AttributeModifier("Backpack bearer health bonus", healthAddition, AttributeModifier.Operation.ADDITION));
 			}
+			monster.setHealth(monster.getMaxHealth());
 		}
 	}
 
@@ -164,8 +165,14 @@ public class EntityBackpackAdditionHandler {
 		return secondaryColor == null ? -1 : secondaryColor;
 	}
 
-	private static final List<Effect> APPLICABLE_EFFECTS = ImmutableList.of(Effects.RESISTANCE, Effects.FIRE_RESISTANCE, Effects.ABSORPTION, Effects.HEALTH_BOOST,
-			Effects.REGENERATION, Effects.SPEED, Effects.STRENGTH);
+	private static final List<ApplicableEffect> APPLICABLE_EFFECTS = ImmutableList.of(
+			new ApplicableEffect(Effects.RESISTANCE, 3),
+			new ApplicableEffect(Effects.FIRE_RESISTANCE),
+			new ApplicableEffect(Effects.ABSORPTION),
+			new ApplicableEffect(Effects.HEALTH_BOOST),
+			new ApplicableEffect(Effects.REGENERATION),
+			new ApplicableEffect(Effects.SPEED),
+			new ApplicableEffect(Effects.STRENGTH));
 
 	private static void setLoot(MonsterEntity monster, IBackpackWrapper backpackWrapper, int difficulty) {
 		MinecraftServer server = monster.world.getServer();
@@ -181,9 +188,9 @@ public class EntityBackpackAdditionHandler {
 	private static void applyPotions(MonsterEntity monster, int difficulty, int minDifficulty) {
 		if (Boolean.TRUE.equals(Config.COMMON.entityBackpackAdditions.buffWithPotionEffects.get())) {
 			RandHelper.getNRandomElements(APPLICABLE_EFFECTS, difficulty + 2)
-					.forEach(effect -> {
-						int amplifier = Math.max(minDifficulty, monster.world.rand.nextInt(difficulty + 1));
-						monster.addPotionEffect(new EffectInstance(effect, 30 * 60 * 20, amplifier));
+					.forEach(applicableEffect -> {
+						int amplifier = Math.min(Math.max(minDifficulty, monster.world.rand.nextInt(difficulty + 1)), applicableEffect.getMaxAmplifier());
+						monster.addPotionEffect(new EffectInstance(applicableEffect.getEffect(), 30 * 60 * 20, amplifier));
 					});
 		}
 	}
@@ -263,6 +270,29 @@ public class EntityBackpackAdditionHandler {
 
 		public int getMinDifficulty() {
 			return minDifficulty;
+		}
+	}
+
+	private static class ApplicableEffect {
+		private final Effect effect;
+
+		private final int maxAmplifier;
+
+		private ApplicableEffect(Effect effect) {
+			this(effect, Integer.MAX_VALUE);
+		}
+
+		private ApplicableEffect(Effect effect, int maxAmplifier) {
+			this.effect = effect;
+			this.maxAmplifier = maxAmplifier;
+		}
+
+		public Effect getEffect() {
+			return effect;
+		}
+
+		public int getMaxAmplifier() {
+			return maxAmplifier;
 		}
 	}
 }
