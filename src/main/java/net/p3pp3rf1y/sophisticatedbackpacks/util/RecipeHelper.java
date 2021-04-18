2efec1b770237a00ea6d49f634e1d14618391f11
P3pp3rF1y <p3pp3rf1y@gmail.com>
2021-04-18 20:30:33 +0200

    fix: 🐛 Fixed compacting recipes looping in case there is a compacting recipe to compact item into itself or there's a chain of compacting recipes which start to loop. This fixes stack overflow crashes that could happen if one of such items got into backpack with compacting upgrade.

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/util/RecipeHelper.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/util/RecipeHelper.java
@@ -19,13 +19,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Queue;
 import java.util.Set;
+
+import static net.p3pp3rf1y.sophisticatedbackpacks.util.RecipeHelper.CompactingShape.*;
 
 public class RecipeHelper {
 	private static final Map<Item, Set<CompactingShape>> ITEM_COMPACTING_SHAPES = new HashMap<>();
+	private static final int MAX_FOLLOW_UP_COMPACTING_RECIPES = 10;
 	private static WeakReference<World> world;
 
 	private RecipeHelper() {}
@@ -41,24 +46,67 @@ public class RecipeHelper {
 	private static Set<CompactingShape> getCompactingShapes(Item item) {
 		return getWorld().map(w -> {
 			Set<CompactingShape> compactingShapes = new HashSet<>();
-			ItemStack compactingResult = getCraftingResultAndRemainingItems(item, w, 2, 2, new ArrayList<>());
-			if (!compactingResult.isEmpty()) {
-				if (uncompactMatchesItem(compactingResult, w, item, 4)) {
-					compactingShapes.add(CompactingShape.TWO_BY_TWO_UNCRAFTABLE);
-				} else {
-					compactingShapes.add(CompactingShape.TWO_BY_TWO);
-				}
-			}
-			compactingResult = getCraftingResultAndRemainingItems(item, w, 3, 3, new ArrayList<>());
-			if (!compactingResult.isEmpty()) {
-				if (uncompactMatchesItem(compactingResult, w, item, 9)) {
-					compactingShapes.add(CompactingShape.THREE_BY_THREE_UNCRAFTABLE);
-				} else {
-					compactingShapes.add(CompactingShape.THREE_BY_THREE);
-				}
-			}
+			getCompactingShape(item, w, 2, 2, TWO_BY_TWO_UNCRAFTABLE, TWO_BY_TWO).ifPresent(compactingShapes::add);
+			getCompactingShape(item, w, 3, 3, THREE_BY_THREE_UNCRAFTABLE, THREE_BY_THREE).ifPresent(compactingShapes::add);
 			return compactingShapes;
 		}).orElse(Collections.emptySet());
+	}
+
+	private static Optional<CompactingShape> getCompactingShape(Item item, World w, int width, int height, CompactingShape uncraftableShape, CompactingShape shape) {
+		ItemStack compactingResult;
+		compactingResult = getCraftingResultAndRemainingItems(item, w, width, height, new ArrayList<>());
+		if (!compactingResult.isEmpty()) {
+			if (item == compactingResult.getItem()) {
+				return Optional.empty();
+			}
+
+			if (isPartOfCompactingLoop(item, compactingResult.getItem(), w)) {
+				return Optional.empty();
+			}
+
+			if (uncompactMatchesItem(compactingResult, w, item, width * height)) {
+				return Optional.of(uncraftableShape);
+			} else {
+				return Optional.of(shape);
+			}
+		}
+		return Optional.empty();
+	}
+
+	private static boolean isPartOfCompactingLoop(Item firstCompacted, Item firstCompactResult, World w) {
+		ItemStack compactingResult;
+		int iterations = 0;
+		Set<Item> compactedItems = new HashSet<>();
+		Queue<Item> itemsToCompact = new LinkedList<>();
+		itemsToCompact.add(firstCompactResult);
+		while (!itemsToCompact.isEmpty()) {
+			Item itemToCompact = itemsToCompact.poll();
+			compactingResult = getCraftingResultAndRemainingItems(itemToCompact, w, 2, 2, new ArrayList<>());
+			if (!compactingResult.isEmpty()) {
+				if (compactingResult.getItem() == firstCompacted) {
+					return true;
+				} else if (compactedItems.contains(compactingResult.getItem())) {
+					return false; //loop exists but the first compacted item isn't part of it so we will let it be compacted, but no follow up compacting will happen
+				}
+				itemsToCompact.add(compactingResult.getItem());
+			}
+
+			compactingResult = getCraftingResultAndRemainingItems(itemToCompact, w, 3, 3, new ArrayList<>());
+			if (!compactingResult.isEmpty()) {
+				if (compactingResult.getItem() == firstCompacted) {
+					return true;
+				} else if (compactedItems.contains(compactingResult.getItem())) {
+					return false; //loop exists but the first compacted item isn't part of it so we will let it be compacted, but no follow up compacting will happen
+				}
+				itemsToCompact.add(compactingResult.getItem());
+			}
+			compactedItems.add(itemToCompact);
+			iterations++;
+			if (iterations > MAX_FOLLOW_UP_COMPACTING_RECIPES) {
+				return true; //we were unable to figure out if the loop exists because of way too many follow up compacting recipe thus not allowing to compact anyway
+			}
+		}
+		return false;
 	}
 
 	private static boolean uncompactMatchesItem(ItemStack result, World w, Item item, int count) {
@@ -108,7 +156,6 @@ public class RecipeHelper {
 	}
 
 	public enum CompactingShape {
-		NONE,
 		THREE_BY_THREE,
 		TWO_BY_TWO,
 		THREE_BY_THREE_UNCRAFTABLE,
