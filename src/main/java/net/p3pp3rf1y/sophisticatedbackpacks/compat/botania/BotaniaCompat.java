c6513e32b028c3c0d8cd603e95b18cf2de375945
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-03-14 12:25:30 +0100

    feat: ✨ Readded compatibility of Magnet Upgrade with Botania's Solegnolia

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/botania/BotaniaCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/botania/BotaniaCompat.java
@@ -1,10 +1,12 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.compat.botania;
 
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.magnet.MagnetUpgradeWrapper;
+import vazkii.botania.api.BotaniaAPI;
 
 public class BotaniaCompat implements ICompat {
 	@Override
 	public void setup() {
-		//MagnetUpgradeWrapper.addMagnetPreventionChecker(entity -> BotaniaAPI.instance().hasSolegnoliaAround(entity));
+		MagnetUpgradeWrapper.addMagnetPreventionChecker(entity -> BotaniaAPI.instance().hasSolegnoliaAround(entity));
 	}
 }
--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/init/ModCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/init/ModCompat.java
@@ -3,6 +3,7 @@ package net.p3pp3rf1y.sophisticatedbackpacks.init;
 import net.minecraftforge.fml.ModList;
 import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
 import net.p3pp3rf1y.sophisticatedbackpacks.compat.CompatModIds;
+import net.p3pp3rf1y.sophisticatedbackpacks.compat.botania.BotaniaCompat;
 import net.p3pp3rf1y.sophisticatedbackpacks.compat.curios.CuriosCompat;
 import net.p3pp3rf1y.sophisticatedbackpacks.compat.inventorysorter.InventorySorterCompat;
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
@@ -20,7 +21,7 @@ public class ModCompat {
 	static {
 		compatFactories.put(CompatModIds.CURIOS, () -> CuriosCompat::new);
 		compatFactories.put(CompatModIds.INVENTORY_SORTER, () -> InventorySorterCompat::new);
-		//compatFactories.put(CompatModIds.BOTANIA, () -> BotaniaCompat::new);
+		compatFactories.put(CompatModIds.BOTANIA, () -> BotaniaCompat::new);
 	}
 
 	public static void initCompats() {
