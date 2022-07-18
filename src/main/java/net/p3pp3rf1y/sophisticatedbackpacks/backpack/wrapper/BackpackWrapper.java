7c159323362e961028332d37d0e29c6797cf116e
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-07-18 17:43:58 +0200

    fix: 🐛 Fixed backpack wrapper to unregister call inventory handler when backpack turns into item form or gets disconnected from controller

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/wrapper/BackpackWrapper.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/wrapper/BackpackWrapper.java
@@ -508,8 +508,13 @@ public class BackpackWrapper implements IBackpackWrapper {
 	}
 
 	@Override
-	public void setOnInventoryHandlerRefreshListener(Runnable onInventoryHandlerRefresh) {
+	public void registerOnInventoryHandlerRefreshListener(Runnable onInventoryHandlerRefresh) {
 		this.onInventoryHandlerRefresh = onInventoryHandlerRefresh;
+	}
+
+	@Override
+	public void unregisterOnInventoryHandlerRefreshListener() {
+		onInventoryHandlerRefresh = () -> {};
 	}
 
 	@Override
