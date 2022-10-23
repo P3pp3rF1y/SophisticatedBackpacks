91c34f14d42271f24006601aacb4d879da343a00
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-10-23 22:07:19 +0200

    fix: 🐛 Fixed NPE that happened when Corail Tombstone's lolipop were picked up by backpack

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/util/ItemStackKey.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/util/ItemStackKey.java
@@ -1,7 +1,13 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.util;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.items.ItemHandlerHelper;
+import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
+
+import javax.annotation.Nullable;
+import java.lang.reflect.Field;
 
 public class ItemStackKey {
 	public ItemStack getStack() {
@@ -16,15 +22,40 @@ public class ItemStackKey {
 
 	@Override
 	public boolean equals(Object o) {
-		if (this == o) { return true; }
-		if (o == null || getClass() != o.getClass()) { return false; }
+		if (this == o) {return true;}
+		if (o == null || getClass() != o.getClass()) {return false;}
 		ItemStackKey that = (ItemStackKey) o;
 		return ItemHandlerHelper.canItemStacksStack(stack, that.stack);
 	}
 
 	@Override
 	public int hashCode() {
-		//noinspection ConstantConditions - hasTag call makes sure getTag doesn't return null
-		return stack.getItem().hashCode() * 31 + (stack.hasTag() ? stack.getTag().hashCode() : 0);
+		return getHashCode(stack);
+	}
+
+	public static int getHashCode(ItemStack stack) {
+		int hash = stack.getItem().hashCode();
+		if (stack.hasTag()) {
+			//noinspection ConstantConditions - hasTag call makes sure getTag doesn't return null
+			hash = hash * 31 + stack.getTag().hashCode();
+		}
+		CompoundNBT capNbt = getCapNbt(stack);
+		if (capNbt != null) {
+			hash = hash * 31 + capNbt.hashCode();
+		}
+		return hash;
+	}
+
+	private static final Field CAP_NBT = ObfuscationReflectionHelper.findField(ItemStack.class, "capNBT");
+
+	@Nullable
+	private static CompoundNBT getCapNbt(ItemStack stack) {
+		try {
+			return (CompoundNBT) CAP_NBT.get(stack);
+		}
+		catch (IllegalAccessException e) {
+			SophisticatedBackpacks.LOGGER.error("Error getting capNBT of stack ", e);
+			return null;
+		}
 	}
 }
