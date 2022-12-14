173333bb8d2799a5e60572b3f5318daa7dc42aed
P3pp3rF1y <p3pp3rf1y@gmail.com>
2022-12-14 13:34:08 +0100

    fix: 🐛 Fixed upgrading refill upgrade to advanced

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/refill/RefillUpgradeTab.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/refill/RefillUpgradeTab.java
@@ -43,72 +43,7 @@ public abstract class RefillUpgradeTab extends UpgradeSettingsTab<RefillUpgradeC
 	protected RefillUpgradeTab(RefillUpgradeContainer upgradeContainer, Position position, StorageScreenBase<?> screen, int slotsInRow, String upgradeName) {
 		super(upgradeContainer, position, screen, SBPTranslationHelper.INSTANCE.translUpgrade(upgradeName), SBPTranslationHelper.INSTANCE.translUpgradeTooltip(upgradeName));
 
-		filterLogicControl = addHideableChild(new FilterLogicControl<>(screen, new Position(x + 3, y + 24), getContainer().getFilterLogicContainer(), slotsInRow) {
-			@Override
-			protected void renderWidget(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
-				super.renderWidget(poseStack, mouseX, mouseY, partialTicks);
-				poseStack.pushPose();
-				poseStack.translate(0, 0, 100);
-				getContainer().getTargetSlots().forEach((slot, ts) -> {
-					RefillUpgradeWrapper.TargetSlot targetSlot = slotBeingChanged == slot ? targetSlotBeingChanged : ts;
-					drawString(poseStack, font, targetSlot.getAcronym(),
-							getX() + (slot % slotsInRow) * 18 + 10, getY() + (slot / slotsInRow) * 18 + 2, DyeColor.GREEN.getTextColor());
-				});
-
-				poseStack.popPose();
-				if (isMouseOver(mouseX, mouseY)) {
-					int slot = getSlot(mouseX, mouseY);
-					if (slotBeingChanged > -1) {
-						updateTooltip(targetSlotBeingChanged);
-					} else {
-						RefillUpgradeWrapper.TargetSlot targetSlot = getContainer().getTargetSlots().get(slot);
-						if (targetSlot != null && (additionalTooltip.isEmpty() || !additionalTooltip.get(0).equals(targetSlot.getDescription()))) {
-							updateTooltip(targetSlot);
-						}
-					}
-				}
-			}
-
-			private void updateTooltip(RefillUpgradeWrapper.TargetSlot targetSlot) {
-				resetAdditionalTooltip();
-				additionalTooltip.add(SBPTranslationHelper.INSTANCE.translUpgrade("refill.target_slot.tooltip", targetSlot.getDescription()).withStyle(ChatFormatting.GRAY));
-				additionalTooltip.add(SCROLL_TOOLTIP);
-			}
-
-			@Override
-			public void render(PoseStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-				super.render(matrixStack, mouseX, mouseY, partialTicks);
-
-				int slot = getSlot(mouseX, mouseY);
-
-				if (slotBeingChanged > -1 && slotBeingChanged != slot) {
-					saveTargetSlot();
-				}
-			}
-
-			@Override
-			public boolean mouseScrolled(double mouseX, double mouseY, double pDelta) {
-				int slot = getSlot(mouseX, mouseY);
-
-				if (slotBeingChanged == -1) {
-					RefillUpgradeWrapper.TargetSlot targetSlot = getContainer().getTargetSlots().get(slot);
-					if (targetSlot != null) {
-						slotBeingChanged = slot;
-						targetSlotBeingChanged = targetSlot;
-					} else {
-						return true;
-					}
-				}
-
-				targetSlotBeingChanged = pDelta > 0 ? targetSlotBeingChanged.next() : targetSlotBeingChanged.previous();
-
-				return true;
-			}
-
-			private int getSlot(double mouseX, double mouseY) {
-				return ((int) mouseX - getX()) / 18 + slotsInRow * (((int) mouseY - getY()) / 18);
-			}
-		});
+		filterLogicControl = addHideableChild(new RefillFilterLogicControl(screen, slotsInRow));
 	}
 
 	@Override
@@ -157,6 +92,91 @@ public abstract class RefillUpgradeTab extends UpgradeSettingsTab<RefillUpgradeC
 	public static class Advanced extends RefillUpgradeTab {
 		public Advanced(RefillUpgradeContainer upgradeContainer, Position position, StorageScreenBase<?> screen, int slotsInRow) {
 			super(upgradeContainer, position, screen, slotsInRow, "advanced_refill");
+		}
+	}
+
+	private class RefillFilterLogicControl extends FilterLogicControl<FilterLogic, FilterLogicContainer<FilterLogic>> {
+		private final int slotsInRow;
+
+		public RefillFilterLogicControl(StorageScreenBase<?> screen, int slotsInRow) {
+			super(screen, new Position(RefillUpgradeTab.this.x + 3, RefillUpgradeTab.this.y + 24), RefillUpgradeTab.this.getContainer().getFilterLogicContainer(), slotsInRow);
+			this.slotsInRow = slotsInRow;
+		}
+
+		@Override
+		protected void renderWidget(PoseStack poseStack, int mouseX, int mouseY, float partialTicks) {
+			super.renderWidget(poseStack, mouseX, mouseY, partialTicks);
+			if (!getContainer().allowsTargetSlotSelection()) {
+				return;
+			}
+
+			renderTargetSlotAcronyms(poseStack);
+			updateTargetSlotTooltip(mouseX, mouseY);
+		}
+
+		private void updateTargetSlotTooltip(int mouseX, int mouseY) {
+			if (isMouseOver(mouseX, mouseY)) {
+				int slot = getSlot(mouseX, mouseY);
+				if (slotBeingChanged > -1) {
+					updateTooltip(targetSlotBeingChanged);
+				} else {
+					RefillUpgradeWrapper.TargetSlot targetSlot = getContainer().getTargetSlot(slot);
+					if (additionalTooltip.isEmpty() || !additionalTooltip.get(0).equals(targetSlot.getDescription())) {
+						updateTooltip(targetSlot);
+					}
+				}
+			}
+		}
+
+		private void renderTargetSlotAcronyms(PoseStack poseStack) {
+			poseStack.pushPose();
+			poseStack.translate(0, 0, 100);
+			getContainer().getSlots().forEach(slot -> {
+				if (!slot.getItem().isEmpty()) {
+					int slotIndex = slot.getSlotIndex();
+					RefillUpgradeWrapper.TargetSlot ts = getContainer().getTargetSlot(slotIndex);
+					RefillUpgradeWrapper.TargetSlot targetSlot = slotBeingChanged == slotIndex ? targetSlotBeingChanged : ts;
+					drawString(poseStack, font, targetSlot.getAcronym(),
+							getX() + (slotIndex % slotsInRow) * 18 + 10, getY() + (slotIndex / slotsInRow) * 18 + 2, DyeColor.GREEN.getTextColor());
+				}
+					});
+
+			poseStack.popPose();
+		}
+
+		private void updateTooltip(RefillUpgradeWrapper.TargetSlot targetSlot) {
+			resetAdditionalTooltip();
+			additionalTooltip.add(SBPTranslationHelper.INSTANCE.translUpgrade("refill.target_slot.tooltip", targetSlot.getDescription()).withStyle(ChatFormatting.GRAY));
+			additionalTooltip.add(SCROLL_TOOLTIP);
+		}
+
+		@Override
+		public void render(PoseStack matrixStack, int mouseX, int mouseY, float partialTicks) {
+			super.render(matrixStack, mouseX, mouseY, partialTicks);
+
+			int slot = getSlot(mouseX, mouseY);
+
+			if (slotBeingChanged > -1 && slotBeingChanged != slot) {
+				saveTargetSlot();
+			}
+		}
+
+		@Override
+		public boolean mouseScrolled(double mouseX, double mouseY, double pDelta) {
+			int slot = getSlot(mouseX, mouseY);
+
+			if (slotBeingChanged == -1) {
+				slotBeingChanged = slot;
+				targetSlotBeingChanged = getContainer().getTargetSlot(slot);
+			}
+
+			targetSlotBeingChanged = pDelta > 0 ? targetSlotBeingChanged.next() : targetSlotBeingChanged.previous();
+
+			return true;
+		}
+
+		private int getSlot(double mouseX, double mouseY) {
+			return ((int) mouseX - getX()) / 18 + slotsInRow * (((int) mouseY - getY()) / 18);
 		}
 	}
 }
