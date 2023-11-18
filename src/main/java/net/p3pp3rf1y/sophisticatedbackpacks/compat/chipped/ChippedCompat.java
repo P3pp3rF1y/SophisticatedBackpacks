P3pp3rF1y/SophisticatedStorage

feat: ✨ Add 7 new upgrades for Chipped workbenches that can be used in upgrade tabs

src/main/java/net/p3pp3rf1y/sophisticatedstorage/compat/chipped/ChippedCompat.java
@@ -1,0 +1,71 @@
+package net.p3pp3rf1y.sophisticatedstorage.compat.chipped;
+
+import earth.terrarium.chipped.registry.ModRecipeTypes;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.RegistryObject;
+import net.p3pp3rf1y.sophisticatedcore.client.gui.StorageScreenBase;
+import net.p3pp3rf1y.sophisticatedcore.client.gui.UpgradeGuiManager;
+import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.Position;
+import net.p3pp3rf1y.sophisticatedcore.common.gui.UpgradeContainerRegistry;
+import net.p3pp3rf1y.sophisticatedcore.common.gui.UpgradeContainerType;
+import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
+import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeContainer;
+import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeItem;
+import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeTab;
+import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeWrapper;
+import net.p3pp3rf1y.sophisticatedstorage.SophisticatedStorage;
+import net.p3pp3rf1y.sophisticatedstorage.client.gui.StorageButtonDefinitions;
+import net.p3pp3rf1y.sophisticatedstorage.init.ModItems;
+
+public class ChippedCompat implements ICompat {
+
+	public static final RegistryObject<BlockTransformationUpgradeItem> BOTANIST_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/botanist_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedStorage.CREATIVE_TAB, ModRecipeTypes.BOTANIST_WORKBENCH_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> GLASSBLOWER_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/glassblower_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedStorage.CREATIVE_TAB, ModRecipeTypes.GLASSBLOWER_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> CARPENTER_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/carpenter_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedStorage.CREATIVE_TAB, ModRecipeTypes.CARPENTERS_TABLE_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> SHEPHERD_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/shepherd_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedStorage.CREATIVE_TAB, ModRecipeTypes.LOOM_TABLE_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> MASON_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/mason_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedStorage.CREATIVE_TAB, ModRecipeTypes.MASON_TABLE_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> PHILOSOPHER_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/philosopher_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedStorage.CREATIVE_TAB, ModRecipeTypes.ALCHEMY_BENCH_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> TINKERER_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/tinkerer_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedStorage.CREATIVE_TAB, ModRecipeTypes.MECHANIST_WORKBENCH_TYPE));
+
+	@Override
+	public void init() {
+		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modBus.addGenericListener(MenuType.class, this::registerContainers);
+	}
+
+	public void registerContainers(RegistryEvent.Register<MenuType<?>> evt) {
+		registerUpgradeContainer(BOTANIST_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(GLASSBLOWER_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(CARPENTER_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(SHEPHERD_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(MASON_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(PHILOSOPHER_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(TINKERER_WORKBENCH_UPGRADE);
+	}
+
+	private void registerUpgradeContainer(RegistryObject<BlockTransformationUpgradeItem> item) {
+		UpgradeContainerType<BlockTransformationUpgradeWrapper, BlockTransformationUpgradeContainer> containerType = new UpgradeContainerType<>(BlockTransformationUpgradeContainer::new);
+		UpgradeContainerRegistry.register(item.getId(), containerType);
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> UpgradeGuiManager.registerTab(containerType, (BlockTransformationUpgradeContainer upgradeContainer, Position position, StorageScreenBase<?> screen) -> {
+			String itemName = item.getId().getPath();
+			return new BlockTransformationUpgradeTab(upgradeContainer, position, screen, StorageButtonDefinitions.SHIFT_CLICK_TARGET, itemName.replace('/', '_').substring(0, itemName.length() - "_upgrade".length()));
+		}));
+	}
+
+	@Override
+	public void setup() {
+		//noop
+	}
+}

src/main/java/net/p3pp3rf1y/sophisticatedstorage/init/ModCompat.java
@@ -4,6 +4,7 @@ import net.minecraftforge.fml.ModList;
 import net.p3pp3rf1y.sophisticatedcore.compat.CompatModIds;
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
 import net.p3pp3rf1y.sophisticatedstorage.SophisticatedStorage;
+import net.p3pp3rf1y.sophisticatedstorage.compat.chipped.ChippedCompat;
 import net.p3pp3rf1y.sophisticatedstorage.compat.quark.QuarkCompat;
 
 import java.util.HashMap;
@@ -16,20 +17,29 @@ public class ModCompat {
 
 	private static final Map<String, Supplier<Callable<ICompat>>> compatFactories = new HashMap<>();
 
+	private static final Map<String, ICompat> loadedCompats = new HashMap<>();
+
 	static {
 		compatFactories.put(CompatModIds.QUARK, () -> QuarkCompat::new);
+		compatFactories.put(CompatModIds.CHIPPED, () -> ChippedCompat::new);
+	}
+
+	public static void compatsSetup() {
+		loadedCompats.values().forEach(ICompat::setup);
 	}
 
 	public static void initCompats() {
 		for (Map.Entry<String, Supplier<Callable<ICompat>>> entry : compatFactories.entrySet()) {
 			if (ModList.get().isLoaded(entry.getKey())) {
 				try {
-					entry.getValue().get().call().setup();
+					loadedCompats.put(entry.getKey(), entry.getValue().get().call());
 				}
 				catch (Exception e) {
 					SophisticatedStorage.LOGGER.error("Error instantiating compatibility ", e);
 				}
 			}
 		}
+
+		loadedCompats.values().forEach(ICompat::init);
 	}
 }

src/main/java/net/p3pp3rf1y/sophisticatedstorage/init/ModItems.java
@@ -98,7 +98,7 @@ import net.p3pp3rf1y.sophisticatedstorage.upgrades.hopper.HopperUpgradeWrapper;
 public class ModItems {
 	private ModItems() {}
 
-	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SophisticatedStorage.MOD_ID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SophisticatedStorage.MOD_ID);
 
 	public static final ResourceLocation STORAGE_UPGRADE_TAG_NAME = new ResourceLocation(SophisticatedStorage.MOD_ID, "upgrade");
 

P3pp3rF1y/SophisticatedBackpacks

src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/chipped/ChippedCompat.java
@@ -1,0 +1,74 @@
+package net.p3pp3rf1y.sophisticatedbackpacks.compat.chipped;
+
+import earth.terrarium.chipped.common.registry.ModRecipeTypes;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegisterEvent;
+import net.minecraftforge.registries.RegistryObject;
+import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
+import net.p3pp3rf1y.sophisticatedbackpacks.client.gui.SBPButtonDefinitions;
+import net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems;
+import net.p3pp3rf1y.sophisticatedcore.client.gui.StorageScreenBase;
+import net.p3pp3rf1y.sophisticatedcore.client.gui.UpgradeGuiManager;
+import net.p3pp3rf1y.sophisticatedcore.client.gui.utils.Position;
+import net.p3pp3rf1y.sophisticatedcore.common.gui.UpgradeContainerRegistry;
+import net.p3pp3rf1y.sophisticatedcore.common.gui.UpgradeContainerType;
+import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
+import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeContainer;
+import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeItem;
+import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeTab;
+import net.p3pp3rf1y.sophisticatedcore.compat.chipped.BlockTransformationUpgradeWrapper;
+
+public class ChippedCompat implements ICompat {
+
+	public static final RegistryObject<BlockTransformationUpgradeItem> BOTANIST_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/botanist_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedBackpacks.ITEM_GROUP, ModRecipeTypes.BOTANIST_WORKBENCH_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> GLASSBLOWER_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/glassblower_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedBackpacks.ITEM_GROUP, ModRecipeTypes.GLASSBLOWER_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> CARPENTER_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/carpenter_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedBackpacks.ITEM_GROUP, ModRecipeTypes.CARPENTERS_TABLE_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> SHEPHERD_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/shepherd_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedBackpacks.ITEM_GROUP, ModRecipeTypes.LOOM_TABLE_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> MASON_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/mason_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedBackpacks.ITEM_GROUP, ModRecipeTypes.MASON_TABLE_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> PHILOSOPHER_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/philosopher_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedBackpacks.ITEM_GROUP, ModRecipeTypes.ALCHEMY_BENCH_TYPE));
+	public static final RegistryObject<BlockTransformationUpgradeItem> TINKERER_WORKBENCH_UPGRADE = ModItems.ITEMS.register("chipped/tinkerer_workbench_upgrade",
+			() -> new BlockTransformationUpgradeItem(SophisticatedBackpacks.ITEM_GROUP, ModRecipeTypes.TINKERING_TABLE_TYPE));
+
+	@Override
+	public void init() {
+		IEventBus modBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modBus.addListener(this::registerContainers);
+	}
+
+	public void registerContainers(RegisterEvent event) {
+		if (!event.getRegistryKey().equals(ForgeRegistries.Keys.MENU_TYPES)) {
+			return;
+		}
+		registerUpgradeContainer(BOTANIST_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(GLASSBLOWER_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(CARPENTER_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(SHEPHERD_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(MASON_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(PHILOSOPHER_WORKBENCH_UPGRADE);
+		registerUpgradeContainer(TINKERER_WORKBENCH_UPGRADE);
+	}
+
+	private void registerUpgradeContainer(RegistryObject<BlockTransformationUpgradeItem> item) {
+		UpgradeContainerType<BlockTransformationUpgradeWrapper, BlockTransformationUpgradeContainer> containerType = new UpgradeContainerType<>(BlockTransformationUpgradeContainer::new);
+		UpgradeContainerRegistry.register(item.getId(), containerType);
+		DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> UpgradeGuiManager.registerTab(containerType, (BlockTransformationUpgradeContainer upgradeContainer, Position position, StorageScreenBase<?> screen) -> {
+			String itemName = item.getId().getPath();
+			return new BlockTransformationUpgradeTab(upgradeContainer, position, screen, SBPButtonDefinitions.SHIFT_CLICK_TARGET, itemName.replace('/', '_').substring(0, itemName.length() - "_upgrade".length()));
+		}));
+	}
+
+	@Override
+	public void setup() {
+		//noop
+	}
+}

src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/init/ModCompat.java
@@ -3,6 +3,7 @@ package net.p3pp3rf1y.sophisticatedbackpacks.init;
 import net.minecraftforge.fml.ModList;
 import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
 import net.p3pp3rf1y.sophisticatedbackpacks.compat.CompatModIds;
+import net.p3pp3rf1y.sophisticatedbackpacks.compat.chipped.ChippedCompat;
 import net.p3pp3rf1y.sophisticatedbackpacks.compat.curios.CuriosCompat;
 import net.p3pp3rf1y.sophisticatedcore.compat.ICompat;
 
@@ -15,22 +16,30 @@ public class ModCompat {
 	private ModCompat() {}
 
 	private static final Map<String, Supplier<Callable<ICompat>>> compatFactories = new HashMap<>();
+	private static final Map<String, ICompat> loadedCompats = new HashMap<>();
 
 	static {
 		compatFactories.put(CompatModIds.CURIOS, () -> CuriosCompat::new);
-		//compatFactories.put(CompatModIds.BOTANIA, () -> BotaniaCompat::new); //TODO readd Botania compat
+		//compatFactories.put(CompatModIds.BOTANIA, () -> BotaniaCompat::new);
+		compatFactories.put(CompatModIds.CHIPPED, () -> ChippedCompat::new);
+	}
+
+	public static void compatsSetup() {
+		loadedCompats.values().forEach(ICompat::setup);
 	}
 
 	public static void initCompats() {
 		for (Map.Entry<String, Supplier<Callable<ICompat>>> entry : compatFactories.entrySet()) {
 			if (ModList.get().isLoaded(entry.getKey())) {
 				try {
-					entry.getValue().get().call().setup();
+					loadedCompats.put(entry.getKey(), entry.getValue().get().call());
 				}
 				catch (Exception e) {
 					SophisticatedBackpacks.LOGGER.error("Error instantiating compatibility ", e);
 				}
 			}
 		}
+
+		loadedCompats.values().forEach(ICompat::init);
 	}
 }

src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/init/ModItems.java
@@ -149,7 +149,7 @@ import net.p3pp3rf1y.sophisticatedcore.util.ItemBase;
 public class ModItems {
 	private ModItems() {}
 
-	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SophisticatedBackpacks.MOD_ID);
+	public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SophisticatedBackpacks.MOD_ID);
 	private static final DeferredRegister<MenuType<?>> MENU_TYPES = DeferredRegister.create(ForgeRegistries.MENU_TYPES, SophisticatedBackpacks.MOD_ID);
 	private static final DeferredRegister<EntityType<?>> ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, SophisticatedBackpacks.MOD_ID);
 