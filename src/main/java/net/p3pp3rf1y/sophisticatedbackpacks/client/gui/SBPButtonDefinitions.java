93ff314df10b20b154a3e8372122e2bcac356f08
frizzle101101 <aaronfritz96@gmail.com>
2024-06-05 20:14:30 -0400

    Changes for Refill Crafting Grid functionality

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/gui/SBPButtonDefinitions.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/gui/SBPButtonDefinitions.java
@@ -28,4 +28,12 @@ public class SBPButtonDefinitions {
 					false, getButtonStateData(new UV(48, 48), Dimension.SQUARE_16, new Position(1, 1),
 							SBPTranslationHelper.INSTANCE.getTranslatedLines(SBPTranslationHelper.INSTANCE.translUpgradeButton("shift_click_into_inventory")))
 			));
+
+	public static final ButtonDefinition.Toggle<Boolean> REFILL_CRAFTING_GRID = net.p3pp3rf1y.sophisticatedcore.client.gui.controls.ButtonDefinitions.createToggleButtonDefinition(
+			Map.of(
+					true, getButtonStateData(new UV(96, 80), Dimension.SQUARE_16, new Position(1, 1),
+							SBPTranslationHelper.INSTANCE.getTranslatedLines(SBPTranslationHelper.INSTANCE.translUpgradeButton("refill_crafting_grid"))),
+					false, getButtonStateData(new UV(112, 64), Dimension.SQUARE_16, new Position(1, 1),
+							SBPTranslationHelper.INSTANCE.getTranslatedLines(SBPTranslationHelper.INSTANCE.translUpgradeButton("do_not_refill_crafting_grid")))
+			));
 }
