086df875596e40297b3fd89335e3ccc4a07e7bed
P3pp3rF1y <p3pp3rf1y@gmail.com>
2024-12-18 20:50:07 +0100

    fix: 🐛 Fixed backpack render data to be properly serialized / deserialized in data components

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/wrapper/BackpackRenderInfo.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/wrapper/BackpackRenderInfo.java
@@ -2,6 +2,7 @@ package net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.component.CustomData;
 import net.p3pp3rf1y.sophisticatedcore.init.ModCoreDataComponents;
 import net.p3pp3rf1y.sophisticatedcore.renderdata.RenderInfo;
 
@@ -19,11 +20,11 @@ public class BackpackRenderInfo extends RenderInfo {
 
 	@Override
 	protected void serializeRenderInfo(CompoundTag renderInfo) {
-		backpack.set(ModCoreDataComponents.RENDER_INFO_TAG, renderInfo);
+		backpack.set(ModCoreDataComponents.RENDER_INFO_TAG, CustomData.of(renderInfo));
 	}
 
 	@Override
 	protected Optional<CompoundTag> getRenderInfoTag() {
-		return Optional.ofNullable(backpack.get(ModCoreDataComponents.RENDER_INFO_TAG));
+		return Optional.ofNullable(backpack.get(ModCoreDataComponents.RENDER_INFO_TAG)).map(CustomData::copyTag);
 	}
 }
