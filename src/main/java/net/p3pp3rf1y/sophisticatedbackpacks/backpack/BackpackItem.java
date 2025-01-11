6507828ab6d9719ed8fd81c77bf6904fa96bd17e
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-01-11 23:49:14 +0100

    fix: 🐛 Fixed stashing to backpack to not work in creative screen because in this screen there's no serverside processing and thus items actually don't get moved to backpack storage

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/BackpackItem.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/BackpackItem.java
@@ -2,6 +2,7 @@ package net.p3pp3rf1y.sophisticatedbackpacks.backpack;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.gui.screens.inventory.CreativeModeInventoryScreen;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
@@ -328,7 +329,7 @@ public class BackpackItem extends ItemBase implements IStashStorageItem {
 
 	@Override
 	public boolean overrideStackedOnOther(ItemStack storageStack, Slot slot, ClickAction action, Player player) {
-		if (storageStack.getCount() > 1 || !slot.mayPickup(player) || action != ClickAction.SECONDARY) {
+		if (hasCreativeScreenContainerOpen(player) || storageStack.getCount() > 1 || !slot.mayPickup(player) || action != ClickAction.SECONDARY) {
 			return super.overrideStackedOnOther(storageStack, slot, action, player);
 		}
 
@@ -352,7 +353,7 @@ public class BackpackItem extends ItemBase implements IStashStorageItem {
 
 	@Override
 	public boolean overrideOtherStackedOnMe(ItemStack storageStack, ItemStack otherStack, Slot slot, ClickAction action, Player player, SlotAccess carriedAccess) {
-		if (storageStack.getCount() > 1 || !slot.mayPlace(storageStack) || action != ClickAction.SECONDARY) {
+		if (hasCreativeScreenContainerOpen(player) || storageStack.getCount() > 1 || !slot.mayPlace(storageStack) || action != ClickAction.SECONDARY) {
 			return super.overrideOtherStackedOnMe(storageStack, otherStack, slot, action, player, carriedAccess);
 		}
 
@@ -364,6 +365,10 @@ public class BackpackItem extends ItemBase implements IStashStorageItem {
 		}
 
 		return super.overrideOtherStackedOnMe(storageStack, otherStack, slot, action, player, carriedAccess);
+	}
+
+	private boolean hasCreativeScreenContainerOpen(Player player) {
+		return player.level().isClientSide() && player.containerMenu instanceof CreativeModeInventoryScreen.ItemPickerMenu;
 	}
 
 	@Override
