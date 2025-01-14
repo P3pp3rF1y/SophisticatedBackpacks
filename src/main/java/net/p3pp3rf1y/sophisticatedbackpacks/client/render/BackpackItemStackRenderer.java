777749da2e28f149dda26f108cb27fd645c6c7f3
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-01-14 15:54:29 +0100

    fix: 🐛 Cleaned up backpack stack rendering so that it would be more compatible with rendering that does different transformations than the default gui ones

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/render/BackpackItemStackRenderer.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/render/BackpackItemStackRenderer.java
@@ -14,7 +14,6 @@ import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
-import net.minecraftforge.client.ForgeHooksClient;
 import net.p3pp3rf1y.sophisticatedbackpacks.api.CapabilityBackpackWrapper;
 
 public class BackpackItemStackRenderer extends BlockEntityWithoutLevelRenderer {
@@ -26,16 +25,8 @@ public class BackpackItemStackRenderer extends BlockEntityWithoutLevelRenderer {
 
 	@Override
 	public void renderByItem(ItemStack stack, ItemDisplayContext transformType, PoseStack matrixStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay) {
-		//ItemRenderer.render does transformations that would need to be transformed against in complicated way so rather pop the pose here and push the new one with the same transforms
-		// applied in the correct order with the getModel
-		matrixStack.popPose();
-		matrixStack.pushPose();
 		ItemRenderer itemRenderer = minecraft.getItemRenderer();
 		BakedModel model = itemRenderer.getModel(stack, null, minecraft.player, 0);
-
-		boolean leftHand = minecraft.player != null && minecraft.player.getOffhandItem() == stack;
-		model = ForgeHooksClient.handleCameraTransforms(matrixStack, model, transformType, leftHand);
-		matrixStack.translate(-0.5D, -0.5D, -0.5D);
 		RenderType rendertype = ItemBlockRenderTypes.getRenderType(stack, true);
 		VertexConsumer ivertexbuilder = ItemRenderer.getFoilBufferDirect(buffer, rendertype, true, stack.hasFoil());
 		itemRenderer.renderModelLists(model, stack, combinedLight, combinedOverlay, matrixStack, ivertexbuilder);
