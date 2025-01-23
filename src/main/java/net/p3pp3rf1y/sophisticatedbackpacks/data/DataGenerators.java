02fbf3a65baeadfa472ec526afcdbd209a835f85
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-01-23 13:21:34 +0100

    feat: ✨ Changed the way items are checked for whether they can go into upgrade slots to only use sophisticatedbackpacks:upgrade tag even for the base mod upgrades so that it's consistent with how these are checked when it comes to upgrades from any addon mods

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/data/DataGenerators.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/data/DataGenerators.java
@@ -1,8 +1,11 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.data;
 
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.data.event.GatherDataEvent;
+import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
 
 public class DataGenerators {
 	private DataGenerators() {}
@@ -10,6 +13,14 @@ public class DataGenerators {
 	public static void gatherData(GatherDataEvent evt) {
 		DataGenerator generator = evt.getGenerator();
 		PackOutput packOutput = generator.getPackOutput();
+		BlockTagsProvider blockTagProvider = new BlockTagsProvider(packOutput, evt.getLookupProvider(), SophisticatedBackpacks.MOD_ID, evt.getExistingFileHelper()){
+			@Override
+			protected void addTags(HolderLookup.Provider pProvider) {
+				//noop
+			}
+		};
+		generator.addProvider(evt.includeServer(), blockTagProvider);
+		generator.addProvider(evt.includeServer(), new ItemTagProvider(packOutput, evt.getLookupProvider(), blockTagProvider.contentsGetter(), evt.getExistingFileHelper()));
 		generator.addProvider(evt.includeServer(), new SBLootTableProvider(packOutput));
 		generator.addProvider(evt.includeServer(), new SBLootModifierProvider(packOutput));
 		generator.addProvider(evt.includeServer(), new SBPRecipeProvider(packOutput));
---
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/data/ItemTagProvider.java
@@ -0,0 +1,30 @@
+package net.p3pp3rf1y.sophisticatedbackpacks.data;
+
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
+import net.p3pp3rf1y.sophisticatedbackpacks.init.ModItems;
+import net.p3pp3rf1y.sophisticatedcore.upgrades.UpgradeItemBase;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+
+public class ItemTagProvider extends ItemTagsProvider {
+	public ItemTagProvider(PackOutput packOutput, CompletableFuture<HolderLookup.Provider> lookupProvider, CompletableFuture<TagLookup<Block>> blockTagProvider, @Nullable ExistingFileHelper existingFileHelper) {
+		super(packOutput, lookupProvider, blockTagProvider, SophisticatedBackpacks.MOD_ID, existingFileHelper);
+	}
+
+	@Override
+	protected void addTags(HolderLookup.Provider pProvider) {
+		IntrinsicTagAppender<Item> upgradeTag = tag(ModItems.BACKPACK_UPGRADE_TAG);
+		ForgeRegistries.ITEMS.getEntries().stream()
+				.filter(entry -> entry.getKey().location().getNamespace().equals(SophisticatedBackpacks.MOD_ID) && entry.getValue() instanceof UpgradeItemBase)
+				.map(Map.Entry::getValue).forEach(upgradeTag::add);
+	}
+}
