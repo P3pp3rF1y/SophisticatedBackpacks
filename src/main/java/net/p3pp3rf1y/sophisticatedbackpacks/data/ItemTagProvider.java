a37d4c5314c8a6d18f2830333f9c9e250ecbba1f
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-01-23 18:34:41 +0100

    fix: 🐛 Fixed Chipped upgrades to be included as optional entries in sophisticatedbackpacks:upgrade tag list (and thus not break loading this list when Chipped isn't present)

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/data/ItemTagProvider.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/data/ItemTagProvider.java
@@ -4,6 +4,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
@@ -25,6 +26,13 @@ public class ItemTagProvider extends ItemTagsProvider {
 		IntrinsicTagAppender<Item> upgradeTag = tag(ModItems.BACKPACK_UPGRADE_TAG);
 		BuiltInRegistries.ITEM.entrySet().stream()
 				.filter(entry -> entry.getKey().location().getNamespace().equals(SophisticatedBackpacks.MOD_ID) && entry.getValue() instanceof UpgradeItemBase)
-				.map(Map.Entry::getValue).forEach(upgradeTag::add);
+				.map(Map.Entry::getValue).forEach(item -> {
+					ResourceLocation location = BuiltInRegistries.ITEM.getKey(item);
+					if (location.getPath().contains("/")) {
+						upgradeTag.addOptional(location);
+					} else {
+						upgradeTag.add(item);
+					}
+				});
 	}
 }
