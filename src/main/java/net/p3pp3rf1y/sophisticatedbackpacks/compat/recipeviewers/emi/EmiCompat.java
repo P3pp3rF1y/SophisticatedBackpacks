608a65bfdab2c9bc104d28f98bd49d2fc0770650
Salandora <salandora@gmail.com>
2025-05-04 17:01:50 +0200

    fix: Fixed a bug where multiple compatibilities would overwrite each others JEIs recipe catalyst registration and EMIs workbench registration

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/recipeviewers/emi/EmiCompat.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/recipeviewers/emi/EmiCompat.java
@@ -27,9 +27,9 @@ import static net.p3pp3rf1y.sophisticatedbackpacks.compat.recipeviewers.common.s
 
 @EmiEntrypoint
 public class EmiCompat implements EmiPlugin {
-	private static Consumer<WorkstationRegistration> additionalWorkstations = (registrar) -> {};
-	public static void setAdditionalWorkstations(Consumer<WorkstationRegistration> additionalWorkstations) {
-		EmiCompat.additionalWorkstations = additionalWorkstations;
+	private static Consumer<WorkstationRegistration> additionalWorkstations = registrar -> {};
+	public static void addAdditionalWorkstations(Consumer<WorkstationRegistration> additionalWorkstations) {
+		EmiCompat.additionalWorkstations = EmiCompat.additionalWorkstations.andThen(additionalWorkstations);
 	}
 
 	public static class WorkstationRegistration {
