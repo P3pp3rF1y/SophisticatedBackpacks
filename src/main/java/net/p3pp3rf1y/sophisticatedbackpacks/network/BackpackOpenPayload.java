5b66d923155e666f07757b6cc54e844542b03e95
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-07-10 19:32:42 +0200

    fix: 🐛 Fixed backpacks nested in another backpack with inception upgrade to properly save their contents id if they haven't been open before being put into the main backpack. Prevents item loss in that case.

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/network/BackpackOpenPayload.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/network/BackpackOpenPayload.java
@@ -8,6 +8,7 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.InventoryMenu;
 import net.neoforged.neoforge.network.handling.IPayloadContext;
 import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
+import net.p3pp3rf1y.sophisticatedbackpacks.backpack.wrapper.BackpackWrapper;
 import net.p3pp3rf1y.sophisticatedbackpacks.common.gui.BackpackContainer;
 import net.p3pp3rf1y.sophisticatedbackpacks.common.gui.BackpackContext;
 import net.p3pp3rf1y.sophisticatedbackpacks.common.gui.IContextAwareContainer;
@@ -63,7 +64,7 @@ public record BackpackOpenPayload(int slotIndex, String identifier, String handl
 			if (payload.slotIndex == -1) {
 				openBackpack(player, backpackContext.getParentBackpackContext());
 			} else if (backpackContainer.isStorageInventorySlot(payload.slotIndex)) {
-				openBackpack(player, backpackContext.getSubBackpackContext(payload.slotIndex));
+				openBackpack(player, backpackContext.getSubBackpackContext(payload.slotIndex, BackpackWrapper.fromStack(backpackContext.getBackpackWrapper(player).getInventoryHandler().getSlotStack(payload.slotIndex)).getContentsUuid().isEmpty()));
 			}
 		} else if (player.containerMenu instanceof IContextAwareContainer contextAwareContainer) {
 			BackpackContext backpackContext = contextAwareContainer.getBackpackContext();
