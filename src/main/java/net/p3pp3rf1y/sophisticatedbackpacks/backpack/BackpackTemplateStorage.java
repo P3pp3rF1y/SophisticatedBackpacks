58480d2a6f03f23d034a88de4e9998fb64e99e88
Salandora <salandora@gmail.com>
2025-09-02 12:03:08 +0200

    feat: ✨ Added template functionality for backpacks useful for starter backpacks, quest backpacks or similar kind of setups. Multiple commands to work with the templates: - /sb template create <templateName> [override]: Creates a template from a backpack held in the main hand - /sb template delete <templateName>: Deletes the specified template - /sb template list Lists all templates - /sb template give <templateName> [targets]: Creates a unique backpack based on the template name specified - /sb template export <templateName>: Exports template as a datapack

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/BackpackTemplateStorage.java
@@ -0,0 +1,78 @@
+package net.p3pp3rf1y.sophisticatedbackpacks.backpack;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.saveddata.SavedData;
+import net.minecraft.world.level.saveddata.SavedDataType;
+import net.minecraft.world.level.storage.DimensionDataStorage;
+import net.neoforged.fml.util.thread.SidedThreadGroups;
+import net.neoforged.neoforge.event.level.LevelEvent;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
+import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
+public class BackpackTemplateStorage extends SavedData {
+	private static final SavedDataType<BackpackTemplateStorage> TYPE = new SavedDataType<>(SophisticatedBackpacks.MOD_ID + "_backpack_templates", BackpackTemplateStorage::new,
+			RecordCodecBuilder.create(builder -> builder.group(
+					Codec.unboundedMap(
+							ResourceLocation.CODEC,
+							CompoundTag.CODEC
+					).fieldOf("backpackTemplates").forGetter(storage -> storage.backpackTemplates)
+			).apply(builder, BackpackTemplateStorage::new))
+	);
+
+	private static final BackpackTemplateStorage clientStorageCopy = new BackpackTemplateStorage();
+	private final Map<ResourceLocation, CompoundTag> backpackTemplates = new HashMap<>();
+
+	private BackpackTemplateStorage(Map<ResourceLocation, CompoundTag> backpackTemplates) {
+		this.backpackTemplates.putAll(backpackTemplates);
+	}
+
+	private BackpackTemplateStorage() {
+	}
+
+	public static BackpackTemplateStorage get() {
+		if (Thread.currentThread().getThreadGroup() == SidedThreadGroups.SERVER) {
+			MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+			if (server != null) {
+				ServerLevel overworld = server.getLevel(Level.OVERWORLD);
+				//noinspection ConstantConditions - by this time overworld is loaded
+				DimensionDataStorage storage = overworld.getDataStorage();
+				return storage.computeIfAbsent(TYPE);
+			}
+		}
+		return clientStorageCopy;
+	}
+
+	public Optional<CompoundTag> getBackpackTemplate(ResourceLocation templateName) {
+		return Optional.ofNullable(backpackTemplates.get(templateName));
+	}
+
+	public void setBackpackTemplate(ResourceLocation templateName, CompoundTag contents) {
+		backpackTemplates.put(templateName, contents);
+		setDirty();
+	}
+
+	public void removeBackpackTemplate(ResourceLocation templateName) {
+		backpackTemplates.remove(templateName);
+		setDirty();
+	}
+
+	public Map<ResourceLocation, CompoundTag> getBackpackTemplates() {
+		return backpackTemplates;
+	}
+
+	public static void onClientWorldLoad(LevelEvent.Load evt) {
+		if (evt.getLevel().isClientSide()) {
+			clientStorageCopy.backpackTemplates.clear();
+		}
+	}
+}
