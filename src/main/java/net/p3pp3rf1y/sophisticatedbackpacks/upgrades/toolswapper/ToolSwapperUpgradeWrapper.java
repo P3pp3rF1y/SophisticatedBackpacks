3e01bcad13f05ac58713311e8659f2cb1adf198e
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-09-11 15:56:48 +0200

    fix: 🐛 Fixed Tool Swapper upgrade to not swap to random items when breaking instantly breakable block with any tool

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/toolswapper/ToolSwapperUpgradeWrapper.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/toolswapper/ToolSwapperUpgradeWrapper.java
@@ -89,7 +89,7 @@ public class ToolSwapperUpgradeWrapper extends UpgradeWrapperBase<ToolSwapperUpg
 
 		double mainToolSpeed = 0;
 		if (isGoodAtBreakingBlock(player, pos, state, mainHandItem)) {
-			if (lastMinedBlock == block) {
+			if (lastMinedBlock == block || state.getDestroySpeed(player.level(), pos) == 0) {
 				return true;
 			}
 			mainToolSpeed = mainHandItem.getDestroySpeed(state);
