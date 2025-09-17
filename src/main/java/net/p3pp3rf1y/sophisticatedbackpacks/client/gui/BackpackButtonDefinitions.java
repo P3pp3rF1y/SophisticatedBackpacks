605be412176ab3c5f7fde50f5d7b472911120060
frizzle101101 <aaronfritz96@gmail.com>
2025-09-17 18:36:29 +0200

    feat: ✨ Added Crafting Upgrade refill functionality - refills the grid from backpack / player with the same items once an item is crafted - new toggle allows switching this on/off (off by default)
    New feature courtesy of frizzle101101, Thank you!

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/gui/BackpackButtonDefinitions.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/gui/BackpackButtonDefinitions.java
@@ -28,4 +28,12 @@ public class BackpackButtonDefinitions {
 					false, getButtonStateData(new UV(48, 48), Dimension.SQUARE_16, new Position(1, 1),
 							BackpackTranslationHelper.INSTANCE.getTranslatedLines(BackpackTranslationHelper.INSTANCE.translUpgradeButton("shift_click_into_inventory")))
 			));
+
+	public static final ButtonDefinition.Toggle<Boolean> REFILL_CRAFTING_GRID = net.p3pp3rf1y.sophisticatedcore.client.gui.controls.ButtonDefinitions.createToggleButtonDefinition(
+			Map.of(
+					true, getButtonStateData(new UV(32, 112), Dimension.SQUARE_16, new Position(1, 1),
+							BackpackTranslationHelper.INSTANCE.getTranslatedLines(BackpackTranslationHelper.INSTANCE.translUpgradeButton("refill_crafting_grid"))),
+					false, getButtonStateData(new UV(48, 112), Dimension.SQUARE_16, new Position(1, 1),
+							BackpackTranslationHelper.INSTANCE.getTranslatedLines(BackpackTranslationHelper.INSTANCE.translUpgradeButton("do_not_refill_crafting_grid")))
+			));
 }
