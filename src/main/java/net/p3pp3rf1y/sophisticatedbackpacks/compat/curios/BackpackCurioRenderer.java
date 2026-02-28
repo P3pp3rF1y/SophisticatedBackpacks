2dc24b1069a63d65cf3836a84f439978f0009ab0
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-02-28 22:00:35 +0100

    feat: ✨ Readded rendering of backpack in curios slot now that Curios updated

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/curios/BackpackCurioRenderer.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/curios/BackpackCurioRenderer.java
@@ -2,29 +2,24 @@ package net.p3pp3rf1y.sophisticatedbackpacks.compat.curios;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.model.EntityModel;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.state.LivingEntityRenderState;
-import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
+import net.p3pp3rf1y.sophisticatedbackpacks.client.render.BackpackLayerRenderer;
 import net.p3pp3rf1y.sophisticatedbackpacks.client.render.BackpackModelManager;
 import net.p3pp3rf1y.sophisticatedbackpacks.client.render.IBackpackModel;
 import top.theillusivec4.curios.api.SlotContext;
 import top.theillusivec4.curios.api.client.ICurioRenderer;
 
-import javax.annotation.Nonnull;
-
 public class BackpackCurioRenderer implements ICurioRenderer {
 	@Override
-	public <S extends LivingEntityRenderState, M extends EntityModel<? super S>> void render(ItemStack stack, SlotContext slotContext, PoseStack poseStack, @Nonnull MultiBufferSource renderTypeBuffer, int packedLight, S renderState, RenderLayerParent<S, M> renderLayerParent, EntityRendererProvider.Context context, float yRotation, float xRotation) {
+	public <S extends LivingEntityRenderState, M extends EntityModel<? super S>> void render(ItemStack stack, SlotContext slotContext, PoseStack poseStack, SubmitNodeCollector submitNodeCollector, int packedLight, S renderState, RenderLayerParent<S, M> renderLayerParent, EntityRendererProvider.Context context, float yRotation, float xRotation) {
 		if (!stack.isEmpty()) {
 			poseStack.pushPose();
 			IBackpackModel model = BackpackModelManager.getBackpackModel(stack.getItem());
-			EquipmentSlot equipmentSlot = model.getRenderEquipmentSlot();
-/* TODO readd with new version of curios
-			BackpackLayerRenderer.submitBackpack(renderLayerParent.getModel(), poseStack, renderTypeBuffer, packedLight, stack, !slotContext.entity().getItemBySlot(equipmentSlot).isEmpty(), BackpackModelManager.getBackpackModel(stack.getItem()), renderState, slotContext.entity().getType(), slotContext.entity().isBaby());
-*/
+			BackpackLayerRenderer.submitBackpack(renderLayerParent.getModel(), renderState, poseStack, submitNodeCollector, packedLight, stack, model);
 			poseStack.popPose();
 		}
 	}
