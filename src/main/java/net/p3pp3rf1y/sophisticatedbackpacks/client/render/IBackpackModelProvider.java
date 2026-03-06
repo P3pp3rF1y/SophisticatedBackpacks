ad2f2a8185e5ebcdcd58bbde88df43d712bbf505
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-03-06 20:13:46 +0100

    refactor: ♻️ Remove unused backpack model classes

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/render/IBackpackModelProvider.java
+++
@@ -1,9 +0,0 @@
-package net.p3pp3rf1y.sophisticatedbackpacks.client.render;
-
-import net.minecraft.world.item.Item;
-
-public interface IBackpackModelProvider {
-	void initModels();
-
-	IBackpackModel getBackpackModel(Item backpackItem);
-}
