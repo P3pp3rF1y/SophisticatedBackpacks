34418970cceca7e6bed5c5accb9ee0643d2d435e
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-03-14 23:07:04 +0100

    fix: 🐛 Fixed rare backpack tank rendering crash with threaded chunk renderers

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/render/BackpackBlockModel.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/client/render/BackpackBlockModel.java
@@ -311,6 +311,7 @@ public class BackpackBlockModel implements UnbakedModel {
 
 		private void addRightSide(QuadCollection.Builder builder, QuadCollection.Builder translucentBuilder) {
 			if (tankRight) {
+				RenderData.TankRenderData rightTankRenderData = this.rightTankRenderData;
 				if (rightTankRenderData != null && rightTankRenderData.fillRatio() != 0) {
 					rightTankRenderData.getFluid().ifPresent(fluid -> {
 						if (cachedRightTankSteps != -1) {
@@ -333,6 +334,7 @@ public class BackpackBlockModel implements UnbakedModel {
 
 		private void addLeftSide(QuadCollection.Builder builder, QuadCollection.Builder translucentBuilder) {
 			if (tankLeft) {
+				RenderData.TankRenderData leftTankRenderData = this.leftTankRenderData;
 				if (leftTankRenderData != null && leftTankRenderData.fillRatio() != 0) {
 					leftTankRenderData.getFluid().ifPresent(fluid -> {
 						if (cachedLeftTankSteps != -1) {
