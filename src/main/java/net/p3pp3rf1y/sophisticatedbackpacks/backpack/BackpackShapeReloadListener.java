4f7111eda1620b592ae4822e262dd2e7199ce47f
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-03-15 23:40:40 +0100

    feat: ✨ Added backpack block bounds derived from model geometry so resource pack backpack models show matching bounds

---
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/BackpackShapeReloadListener.java
@@ -0,0 +1,25 @@
+package net.p3pp3rf1y.sophisticatedbackpacks.backpack;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.server.packs.resources.SimplePreparableReloadListener;
+import net.minecraft.util.profiling.ProfilerFiller;
+import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
+
+public class BackpackShapeReloadListener extends SimplePreparableReloadListener<BackpackShapes.IShapeProvider> {
+	public static final ResourceLocation KEY = SophisticatedBackpacks.getRL("backpack_shape_loader");
+	public static final BackpackShapeReloadListener INSTANCE = new BackpackShapeReloadListener();
+
+	private BackpackShapeReloadListener() {
+	}
+
+	@Override
+	protected BackpackShapes.IShapeProvider prepare(ResourceManager resourceManager, ProfilerFiller profiler) {
+		return BackpackShapes.createDefaultShapeProvider(resourceManager);
+	}
+
+	@Override
+	protected void apply(BackpackShapes.IShapeProvider provider, ResourceManager resourceManager, ProfilerFiller profiler) {
+		BackpackShapes.applyDefaultShapeProvider(provider);
+	}
+}
