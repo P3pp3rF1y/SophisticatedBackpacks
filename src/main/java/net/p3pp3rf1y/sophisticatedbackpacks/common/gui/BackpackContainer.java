762c31752031acf4ab9da499b296700263a84979
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-03-31 23:18:49 +0200

    feat: ✨ Added observer updates when a backpack placed in the world opens or closes

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/gui/BackpackContainer.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/gui/BackpackContainer.java
@@ -11,6 +11,7 @@ import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackAccessLogger;
+import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackBlockEntity;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackItem;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackStorage;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.UUIDDeduplicator;
@@ -22,6 +23,7 @@ import net.p3pp3rf1y.sophisticatedcore.common.gui.SophisticatedMenuProvider;
 import net.p3pp3rf1y.sophisticatedcore.common.gui.StorageContainerMenuBase;
 import net.p3pp3rf1y.sophisticatedcore.inventory.ContainerContents;
 import net.p3pp3rf1y.sophisticatedcore.util.NoopStorageWrapper;
+import net.p3pp3rf1y.sophisticatedcore.util.WorldHelper;
 
 import java.util.Optional;
 
@@ -47,6 +49,11 @@ public class BackpackContainer extends StorageContainerMenuBase<IBackpackWrapper
 
 		if (backpackContext.shouldSaveAfterOpen()) {
 			backpackContext.saveBackpackStack();
+		}
+
+		if (!player.level().isClientSide()) {
+			getBlockPosition().flatMap(pos -> WorldHelper.getBlockEntity(player.level(), pos, BackpackBlockEntity.class))
+					.ifPresent(backpackBlockEntity -> backpackBlockEntity.startOpen(player));
 		}
 	}
 
@@ -81,6 +88,16 @@ public class BackpackContainer extends StorageContainerMenuBase<IBackpackWrapper
 	@Override
 	public boolean stillValid(Player player) {
 		return backpackContext.canInteractWith(player);
+	}
+
+	@Override
+	public void removed(Player player) {
+		if (!player.level().isClientSide()) {
+			getBlockPosition().flatMap(pos -> WorldHelper.getBlockEntity(player.level(), pos, BackpackBlockEntity.class))
+					.ifPresent(backpackBlockEntity -> backpackBlockEntity.stopOpen(player));
+		}
+
+		super.removed(player);
 	}
 
 	public static BackpackContainer fromBuffer(int windowId, Inventory playerInventory, FriendlyByteBuf buffer) {
