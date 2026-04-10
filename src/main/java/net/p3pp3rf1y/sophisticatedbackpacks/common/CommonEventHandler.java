c5e762801c19940afeda2635b9d43834f5fc14e2
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-10 21:34:59 +0200

    fix: 🐛 Fixed backpack mob conversions keeping their special drop state

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/CommonEventHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/CommonEventHandler.java
@@ -24,6 +24,7 @@ import net.neoforged.neoforge.common.util.TriState;
 import net.neoforged.neoforge.event.entity.EntityLeaveLevelEvent;
 import net.neoforged.neoforge.event.entity.EntityMobGriefingEvent;
 import net.neoforged.neoforge.event.entity.living.FinalizeSpawnEvent;
+import net.neoforged.neoforge.event.entity.living.LivingConversionEvent;
 import net.neoforged.neoforge.event.entity.living.LivingDropsEvent;
 import net.neoforged.neoforge.event.entity.player.AttackEntityEvent;
 import net.neoforged.neoforge.event.entity.player.ItemEntityPickupEvent;
@@ -64,6 +65,8 @@ public class CommonEventHandler {
 		IEventBus eventBus = NeoForge.EVENT_BUS;
 		eventBus.addListener(this::onItemPickup);
 		eventBus.addListener(this::onLivingSpecialSpawn);
+		eventBus.addListener(this::onLivingConversionPre);
+		eventBus.addListener(this::onLivingConversion);
 		eventBus.addListener(this::onLivingDrops);
 		eventBus.addListener(this::onEntityMobGriefing);
 		eventBus.addListener(this::onEntityLeaveWorld);
@@ -210,6 +213,14 @@ public class CommonEventHandler {
 
 	private void onLivingDrops(LivingDropsEvent event) {
 		EntityBackpackAdditionHandler.handleBackpackDrop(event);
+	}
+
+	private void onLivingConversionPre(LivingConversionEvent.Pre event) {
+		EntityBackpackAdditionHandler.handleLivingConversionPre(event);
+	}
+
+	private void onLivingConversion(LivingConversionEvent.Post event) {
+		EntityBackpackAdditionHandler.handleLivingConversion(event);
 	}
 
 	private void onEntityMobGriefing(EntityMobGriefingEvent event) {
