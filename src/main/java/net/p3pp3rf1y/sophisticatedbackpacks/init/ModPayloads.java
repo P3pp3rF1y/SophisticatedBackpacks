889e09a2c7d96eda7f7480ec50167aac9bb7dbd2
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-29 23:41:17 +0200

    chore: 🔧 Use mod version for payload registration

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/init/ModPayloads.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/init/ModPayloads.java
@@ -10,7 +10,7 @@ public class ModPayloads {
 	}
 
 	public static void registerPayloads(final RegisterPayloadHandlersEvent event) {
-		final PayloadRegistrar registrar = event.registrar(SophisticatedBackpacks.MOD_ID).versioned("1.0");
+		final PayloadRegistrar registrar = event.registrar(SophisticatedBackpacks.MOD_ID).versioned(SophisticatedBackpacks.getNetworkProtocolVersion());
 		registrar.playToServer(BackpackOpenPayload.TYPE, BackpackOpenPayload.STREAM_CODEC, BackpackOpenPayload::handlePayload);
 		registrar.playToServer(UpgradeTogglePayload.TYPE, UpgradeTogglePayload.STREAM_CODEC, UpgradeTogglePayload::handlePayload);
 		registrar.playToServer(RequestBackpackInventoryContentsPayload.TYPE, RequestBackpackInventoryContentsPayload.STREAM_CODEC, RequestBackpackInventoryContentsPayload::handlePayload);
