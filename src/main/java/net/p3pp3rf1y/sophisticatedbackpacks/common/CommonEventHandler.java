2ffb981f2111ab54232b1822054559856cb092c0
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-05-23 00:48:13 +0200

    fix: 🐛 Fixed backpack addition during async spawns

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/CommonEventHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/common/CommonEventHandler.java
@@ -207,7 +207,7 @@ public class CommonEventHandler {
 	private void onLivingSpecialSpawn(FinalizeSpawnEvent event) {
 		Entity entity = event.getEntity();
 		if (entity instanceof Monster monster && monster.getItemBySlot(EquipmentSlot.CHEST).isEmpty()) {
-			EntityBackpackAdditionHandler.addBackpack(monster, event.getLevel());
+			EntityBackpackAdditionHandler.handleBackpackAdditionOnSpawn(monster, event.getLevel());
 		}
 	}
 
