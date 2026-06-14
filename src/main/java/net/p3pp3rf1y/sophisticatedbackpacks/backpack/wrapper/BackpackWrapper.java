b88b1664c830c982fbeedf0e893ecb0aa33be526
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-14 13:58:04 +0200

    fix: 🐛 Fixed crash when UUID deduplication triggered for item entities around dropped backpack

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/wrapper/BackpackWrapper.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/wrapper/BackpackWrapper.java
@@ -95,6 +95,10 @@ public class BackpackWrapper implements IBackpackWrapper {
 	}
 
 	public static IBackpackWrapper fromStack(ItemStack stack) {
+		if (!(stack.getItem() instanceof BackpackItem)) {
+			return Noop.INSTANCE;
+		}
+
 		if (!stack.has(ModCoreDataComponents.STORAGE_UUID)) {
 			IBackpackWrapper wrapper = new BackpackWrapper(stack);
 			if (stack.has(ModCoreDataComponents.STORAGE_UUID)) {
