e7b265df4e33c6cc00c4ca9bbccdc50cf5c32cd4
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-29 23:42:44 +0200

    fix: 🐛 Fixed mob catcher capturing non-capturable mobs

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/mobcatcher/MobCatcherHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/mobcatcher/MobCatcherHandler.java
@@ -29,6 +29,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.network.PacketDistributor;
 import net.p3pp3rf1y.sophisticatedbackpacks.Config;
 import net.p3pp3rf1y.sophisticatedbackpacks.backpack.BackpackItem;
@@ -112,6 +113,9 @@ public class MobCatcherHandler {
 		}
 		if (entity instanceof EnderDragon || entity instanceof WitherBoss) {
 			return Optional.of(Component.translatable("gui.sophisticatedbackpacks.status.mob_catcher_boss_blocked"));
+		}
+		if (entity.getType().is(Tags.EntityTypes.CAPTURING_NOT_SUPPORTED)) {
+			return Optional.of(Component.translatable("gui.sophisticatedbackpacks.status.mob_catcher_blocklisted"));
 		}
 		if (entity.isPassenger() || entity.isVehicle()) {
 			return Optional.of(Component.translatable("gui.sophisticatedbackpacks.status.mob_catcher_passengers_blocked"));
