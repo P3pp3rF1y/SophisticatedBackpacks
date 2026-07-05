31e210327b0ff6ac25b935f6b947c2d089ed9b1d
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-05 20:56:38 +0200

    fix: 🐛 Fixed JEI transfers into crafting upgrades

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/recipeviewers/common/BackpackSmithingUpgradeDisplayRecipe.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/compat/recipeviewers/common/BackpackSmithingUpgradeDisplayRecipe.java
@@ -40,7 +40,7 @@ public record BackpackSmithingUpgradeDisplayRecipe(Identifier id, RecipeHolder<S
 	private static BackpackTierUpgradeVariantPair withComponentsFromSource(BackpackTierUpgradeVariantPair pair, ItemStack sourceStack) {
 		ItemStack source = pair.source().copy();
 		copyRenderComponents(sourceStack, source);
-		setSlotNumbers(source);
+		copySlotComponents(sourceStack, source);
 		ItemStack result = pair.result().copy();
 		copyRenderComponents(sourceStack, result);
 		setSlotNumbers(result);
@@ -66,6 +66,11 @@ public record BackpackSmithingUpgradeDisplayRecipe(Identifier id, RecipeHolder<S
 		copyComponent(from, to, ModCoreDataComponents.MAIN_COLOR);
 		copyComponent(from, to, ModCoreDataComponents.ACCENT_COLOR);
 		copyComponent(from, to, ModCoreDataComponents.RENDER_DATA);
+	}
+
+	private static void copySlotComponents(ItemStack from, ItemStack to) {
+		copyComponent(from, to, ModCoreDataComponents.NUMBER_OF_INVENTORY_SLOTS);
+		copyComponent(from, to, ModCoreDataComponents.NUMBER_OF_UPGRADE_SLOTS);
 	}
 
 	private static <T> void copyComponent(ItemStack from, ItemStack to, Supplier<DataComponentType<T>> componentSupplier) {
