d8e3cdd3c18b9c246c8dc6b5650c1d852ef7f7df
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-22 18:27:30 +0200

    fix: 🐛 Fixed upgrade cache concurrent modification crashes

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/inception/InceptionWrapperAccessor.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/upgrades/inception/InceptionWrapperAccessor.java
@@ -4,10 +4,11 @@ import net.p3pp3rf1y.sophisticatedcore.api.IStorageWrapper;
 import net.p3pp3rf1y.sophisticatedcore.upgrades.IUpgradeWrapperAccessor;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class InceptionWrapperAccessor implements IUpgradeWrapperAccessor {
-	private final Map<Class<?>, List<?>> interfaceWrappers = new HashMap<>();
-	private final Map<Class<?>, List<?>> mainBackpackInterfaceWrappers = new HashMap<>();
+	private final Map<Class<?>, List<?>> interfaceWrappers = new ConcurrentHashMap<>();
+	private final Map<Class<?>, List<?>> mainBackpackInterfaceWrappers = new ConcurrentHashMap<>();
 	private final IStorageWrapper backpackWrapper;
 	private final SubBackpacksHandler subBackpacksHandler;
 
