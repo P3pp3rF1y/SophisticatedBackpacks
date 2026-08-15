7f36687b438de94dc33489d19feb023647bfa072
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-15 19:54:32 +0200

    fix: 🐛 Fixed unsafe network packet handling

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/network/SBPPacketHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/network/SBPPacketHandler.java
@@ -1,5 +1,6 @@
 package net.p3pp3rf1y.sophisticatedbackpacks.network;
 
+import net.minecraftforge.network.NetworkDirection;
 import net.p3pp3rf1y.sophisticatedbackpacks.SophisticatedBackpacks;
 import net.p3pp3rf1y.sophisticatedcore.network.PacketHandler;
 
@@ -13,24 +14,32 @@ public class SBPPacketHandler extends PacketHandler {
 	@SuppressWarnings({"java:S2440", "InstantiationOfUtilityClass"})
 	@Override
 	public void registerMessages() {
-		registerMessage(BackpackOpenMessage.class, BackpackOpenMessage::encode, BackpackOpenMessage::decode, BackpackOpenMessage::onMessage);
-		registerMessage(UpgradeToggleMessage.class, UpgradeToggleMessage::encode, UpgradeToggleMessage::decode, UpgradeToggleMessage::onMessage);
+		registerMessage(BackpackOpenMessage.class, BackpackOpenMessage::encode, BackpackOpenMessage::decode, BackpackOpenMessage::onMessage,
+				NetworkDirection.PLAY_TO_SERVER);
+		registerMessage(UpgradeToggleMessage.class, UpgradeToggleMessage::encode, UpgradeToggleMessage::decode, UpgradeToggleMessage::onMessage,
+				NetworkDirection.PLAY_TO_SERVER);
 		registerMessage(RequestBackpackInventoryContentsMessage.class, RequestBackpackInventoryContentsMessage::encode,
-				RequestBackpackInventoryContentsMessage::decode, RequestBackpackInventoryContentsMessage::onMessage);
-		registerMessage(BackpackContentsMessage.class, BackpackContentsMessage::encode, BackpackContentsMessage::decode, BackpackContentsMessage::onMessage);
+				RequestBackpackInventoryContentsMessage::decode, RequestBackpackInventoryContentsMessage::onMessage, NetworkDirection.PLAY_TO_SERVER);
+		registerMessage(BackpackContentsMessage.class, BackpackContentsMessage::encode, BackpackContentsMessage::decode, BackpackContentsMessage::onMessage,
+				NetworkDirection.PLAY_TO_CLIENT);
 		registerMessage(InventoryInteractionMessage.class, InventoryInteractionMessage::encode, InventoryInteractionMessage::decode,
-				InventoryInteractionMessage::onMessage);
-		registerMessage(BlockToolSwapMessage.class, BlockToolSwapMessage::encode, BlockToolSwapMessage::decode, BlockToolSwapMessage::onMessage);
-		registerMessage(EntityToolSwapMessage.class, EntityToolSwapMessage::encode, EntityToolSwapMessage::decode, EntityToolSwapMessage::onMessage);
+				InventoryInteractionMessage::onMessage, NetworkDirection.PLAY_TO_SERVER);
+		registerMessage(BlockToolSwapMessage.class, BlockToolSwapMessage::encode, BlockToolSwapMessage::decode, BlockToolSwapMessage::onMessage,
+				NetworkDirection.PLAY_TO_SERVER);
+		registerMessage(EntityToolSwapMessage.class, EntityToolSwapMessage::encode, EntityToolSwapMessage::decode, EntityToolSwapMessage::onMessage,
+				NetworkDirection.PLAY_TO_SERVER);
 		registerMessage(BackpackCloseMessage.class, (backpackCloseMessage, packetBuffer) -> {
-		}, packetBuffer -> new BackpackCloseMessage(), (backpackCloseMessage, contextSupplier) -> BackpackCloseMessage.onMessage(contextSupplier));
-		registerMessage(SyncClientInfoMessage.class, SyncClientInfoMessage::encode, SyncClientInfoMessage::decode, SyncClientInfoMessage::onMessage);
+		}, packetBuffer -> new BackpackCloseMessage(), (backpackCloseMessage, contextSupplier) -> BackpackCloseMessage.onMessage(contextSupplier),
+				NetworkDirection.PLAY_TO_SERVER);
+		registerMessage(SyncClientInfoMessage.class, SyncClientInfoMessage::encode, SyncClientInfoMessage::decode, SyncClientInfoMessage::onMessage,
+				NetworkDirection.PLAY_TO_CLIENT);
 		registerMessage(AnotherPlayerBackpackOpenMessage.class, AnotherPlayerBackpackOpenMessage::encode, AnotherPlayerBackpackOpenMessage::decode,
-				AnotherPlayerBackpackOpenMessage::onMessage);
-		registerMessage(BlockPickMessage.class, BlockPickMessage::encode, BlockPickMessage::decode, BlockPickMessage::onMessage);
+				AnotherPlayerBackpackOpenMessage::onMessage, NetworkDirection.PLAY_TO_SERVER);
+		registerMessage(BlockPickMessage.class, BlockPickMessage::encode, BlockPickMessage::decode, BlockPickMessage::onMessage,
+				NetworkDirection.PLAY_TO_SERVER);
 		registerMessage(MobCatcherCaptureEffectMessage.class, MobCatcherCaptureEffectMessage::encode, MobCatcherCaptureEffectMessage::decode,
-				MobCatcherCaptureEffectMessage::onMessage);
-		registerMessage(MobCatcherReleaseMessage.class, MobCatcherReleaseMessage::encode, MobCatcherReleaseMessage::decode,
-				MobCatcherReleaseMessage::onMessage);
+				MobCatcherCaptureEffectMessage::onMessage, NetworkDirection.PLAY_TO_CLIENT);
+		registerMessage(MobCatcherReleaseMessage.class, MobCatcherReleaseMessage::encode, MobCatcherReleaseMessage::decode, MobCatcherReleaseMessage::onMessage,
+				NetworkDirection.PLAY_TO_SERVER);
 	}
 }
