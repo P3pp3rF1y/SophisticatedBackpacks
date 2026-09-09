95038f2fd88a2f24e96888673eec781775890e17
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-09-09 20:34:43 +0200

    fix: 🐛 Fixed stack overflow when Create Packagers access Inception backpacks

--- src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/wrapper/InventoryModificationHandler.java
+++ src/main/java/net/p3pp3rf1y/sophisticatedbackpacks/backpack/wrapper/InventoryModificationHandler.java
@@ -16,7 +16,9 @@ public class InventoryModificationHandler {
 
 	public ITrackedContentsItemResourceHandler getModifiedInventoryHandler() {
 		if (modifiedInventoryHandler == null) {
-			initializeWrappedInventory(backpackWrapper.getInventoryHandler());
+			ITrackedContentsItemResourceHandler inventoryHandler = backpackWrapper.getInventoryHandler();
+			modifiedInventoryHandler = inventoryHandler;
+			initializeWrappedInventory(inventoryHandler);
 		}
 		return modifiedInventoryHandler;
 	}
